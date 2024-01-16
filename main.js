163c8c0c4430197ee4948158788a6ff99b5954a0
Anastasiia Perederii <anastasiia.perederii@gmail.com>
2024-01-16 20:47:02 +0200

    Version1

---
+++ main.js
@@ -0,0 +1,7 @@
+let firstNumber = +prompt('Enter your first number:');
+let secondNumber = +prompt('Enter your second number:');
+let thirdNumber = +prompt('Enter your third number:');
+
+let averageNumber = (firstNumber + secondNumber + thirdNumber)/3;
+
+alert(`Your average number is ${averageNumber}`)
