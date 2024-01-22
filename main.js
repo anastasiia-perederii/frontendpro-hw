89909c35fc1f0d0861b56261b7380f3cb24d4443
Anastasiia Perederii <anastasiia.perederii@gmail.com>
2024-01-22 14:22:50 +0200

    Version1

---
+++ main.js
@@ -0,0 +1,36 @@
+let firstNumber = +prompt('Введіть перше число:');
+let secondNumber = +prompt('Введіть друге число:');
+let sign = prompt('Введіть операцію над числами: +, -, *, /');
+
+// через switch...case
+switch (true) {
+    case sign === '+':
+        console.log(`Ваше число: ${firstNumber + secondNumber}`);
+        break;
+    case sign === '-':
+        console.log(`Ваше число: ${firstNumber - secondNumber}`);
+        break;
+    case sign === '*':
+        console.log(`Ваше число: ${firstNumber * secondNumber}`);
+        break;
+    case sign === '/' && secondNumber !== 0:
+        console.log(`Ваше число: ${firstNumber / secondNumber}`);
+        break;
+    default:
+        console.log("Ви допустили помилку! Спробуйте ще раз.");
+}
+
+// через if...else
+/*
+if(sign === '+') {
+    console.log(`Ваше число: ${firstNumber + secondNumber}`);
+} else if(sign === '-') {
+    console.log(`Ваше число: ${firstNumber - secondNumber}`);
+} else if(sign === '*') {
+    console.log(`Ваше число: ${firstNumber * secondNumber}`);
+} else if(sign === '/') {
+    console.log(`Ваше число: ${secondNumber === 0 ? 'Помилка! На 0 ділити не можна.' : firstNumber / secondNumber}`);
+} else {
+    console.log('Ви допустили помилку! Спробуйте ще раз.');
+}
+*/
