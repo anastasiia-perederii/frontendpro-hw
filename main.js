7059f3a7119f95a5afd8cdc67c5ac68e72b83c46
Anastasiia Perederii <anastasiia.perederii@gmail.com>
2024-01-26 22:36:07 +0200

    Version1

---
+++ main.js
@@ -0,0 +1,7 @@
+let myQuestion = +prompt('Введіть кількість годин:');
+let a = myQuestion * 3600;
+
+alert(myQuestion === 1 ? `В ${myQuestion} годині ${a} секунд` : `В ${myQuestion} годинах ${a} секунд`)
+
+
+
