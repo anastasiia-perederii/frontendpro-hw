634ed0f49aac752fecb12c03cd6ebe2f258317c1
Anastasiia Perederii <anastasiia.perederii@gmail.com>
2024-02-19 23:01:36 +0200

    version4

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 const controls = document.querySelectorAll('.controls');
 const shownImage = document.querySelector('.shownImage');
 let currentSlide = 0;
+let images = ['1.png', '2.png', '3.png', '4.png', '5.png'];
 
 const loadSlide = () => {
-    shownImage.src = `./img/${currentSlide + 1}.png`;
+    shownImage.src =  `./img/${images[currentSlide]}`;
 };
 
-const goToSlide = (n) => {
-    currentSlide = (n + 5) % 5;
+const goToSlide = () => {
     loadSlide();
     document.getElementById('previous').style.display = (currentSlide === 0) ? 'none' : 'inline-block';
-    document.getElementById('next').style.display = (currentSlide === 4) ? 'none' : 'inline-block';
+    document.getElementById('next').style.display = (currentSlide === images.length - 1) ? 'none' : 'inline-block';
 };
-
 controls.forEach(control => control
     .addEventListener('click', () =>
-        goToSlide(currentSlide + (control.id === 'next' ? 1 : -1))));
+        goToSlide(control.id === 'next' ? currentSlide++ : currentSlide--)));
 
 document.getElementById('previous').style.display = 'none';
 
